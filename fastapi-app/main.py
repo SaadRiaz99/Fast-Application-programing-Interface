aa3802f914a33bd2495453f58b52f833c934b505
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-18 23:37:00 -0700

    Change with Testing

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello I am Saad Bin Riaz")
+# from fastapi import FastApi 
 
 
-if __name__ == "__main__":
-    main()
+# app = fastApi()
+
+
+# @app.get("/")
+# def main():
+#     return {"message": "Hello I am saad Bin Riaz"}
+
+
+def main(firstname : str , lastname : str ):
+    firstname.capitalize()
+    return firstname + lastname
+
+
+first = "Saad"
+
+last = "Riaz"
+a = main(first , last)
+print(a)
