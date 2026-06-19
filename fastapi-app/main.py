53db751396439e16916dd988b582f0d2599724e4
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-19 00:12:27 -0700

    Add Template Folder and Static Foolder wtih template code

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -25,7 +25,13 @@ from pydantic import BaseModel
 # from pydantic import BaseModel
 from fastapi import jinja2
 
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
 app = FastAPI()
+templates = Jinja2Templates(directory = "template")
 class user(BaseModel):
     name : str
     Age : int
