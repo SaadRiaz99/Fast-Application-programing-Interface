6e2b207b32629bfcd36f4b616b85b1aa90226942
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-19 00:16:16 -0700

    Changes

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -28,7 +28,7 @@ from fastapi import jinja2
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory = "template")
