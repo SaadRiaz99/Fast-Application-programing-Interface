98f3338744cb45db5a858fc3eb35a308deaec453
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-18 23:42:17 -0700

    Run a Fastapi ip

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -9,13 +9,23 @@
 #     return {"message": "Hello I am saad Bin Riaz"}
 
 
-def main(firstname : str , lastname : str ):
-    firstname.capitalize()
-    return firstname + lastname
+# def main(firstname : str , lastname : str ):
+#     firstname.capitalize()
+#     return firstname + lastname
 
 
-first = "Saad"
+# first = "Saad"
 
-last = "Riaz"
-a = main(first , last)
-print(a)
+# last = "Riaz"
+# a = main(first , last)
+# print(a)
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+
+def main():
+    return "I am Saad Bin Riaz and learn a fastapi "
