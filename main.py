5d0b2efb003ac89093984f562dd92d6076dbb359
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-20 11:11:41 -0700

    Remove duplicate imports and unused BaseModel import

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-# from 
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="template") 
+@app.get("/home")
+# def main():
+#     print("Hello I am Saad Bin Riaz I engineer")
+
+
+# if __name__ == "__main__":
+#     main()
 @app.get("/")
-def main():
-    print("Hello I am Saad Bin Riaz I engineer")
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "landing.html",
+        {"request": request}
+    )
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/dashboard")
+async def dashboard(request: Request):
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {"request": request}
+    )
+
+
+@app.get("/login")
+async def login(request: Request):
+    return templates.TemplateResponse(
+        "login.html",
+        {"request": request}
+    )
