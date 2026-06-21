c973da712ee2298259fd01d8efb9675df5a21bd4
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-21 04:43:19 -0700

    Add Base declarative class and get_db dependency

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from urllib.parse import quote_plus
 
-DATABASE_URL = "mysql+pymysql://username:password@localhost:3306/mydatabase"
+password = quote_plus("24022007@")
+DATABASE_URL = f"mysql+pymysql://root:{password}@localhost:3306/Printing"
 
-engine = create_engine()
+engine = create_engine(DATABASE_URL)
 
-local_session = sessionmaker(autoflush = False , autocommit = False ,  bind = engine  )
+local_session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+def get_db():
+    db = local_session()
+    try:
+        yield db
+    finally:
+        db.close()
