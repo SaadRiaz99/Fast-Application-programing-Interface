f90e441289f4cf31f5bfd4d23dc595f1537a1d34
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-06-21 00:05:56 -0700

    Add url in database.py and create a enigne

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
-# local_session = sessionmaker()
+DATABASE_URL = "mysql+pymysql://username:password@localhost:3306/mydatabase"
+
+engine = create_engine()
+
+local_session = sessionmaker(autoflush = False , autocommit = False ,  bind = engine  )
